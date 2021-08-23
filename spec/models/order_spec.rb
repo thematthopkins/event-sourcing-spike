bd64338a44fb8119f2e55798e934376fdb4d7d23
Matthew Hopkins <thematthopkins@gmail.com>
2021-08-23 15:49:43 -0400

    removed class

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,30 +1,19 @@
 require 'rails_helper'
 
-class OrderHandler
-  attr_accessor :events
-
-  def initialize
-    @events = []
-  end
-
-  def call(event)
-    puts "called with event #{event}"
-    events.append(event)
-  end
-end
-
 RSpec.describe Order, type: :model do
   it 'calls subscribed notifications on event publication' do
     event_store = Rails.configuration.event_store
-    orderHandler = OrderHandler.new
-    event_store.subscribe(orderHandler, to: [CreateCart])
+    events_received = []
+    event_store.subscribe(-> (event){
+      events_received.append(event)
+    }, to: [CreateCart])
     event = CreateCart.new(
       data: {
         id: 'my-cart-id'
       }
     )
     event_store.publish(event, stream_name: 'my-stream', expected_version: 10)
-    expect(orderHandler.events)
+    expect(events_received)
       .to eq([event])
   end
 end
