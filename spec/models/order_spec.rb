e28d8324814cd3165b9c21b9dc070960c5b07f99
Matthew Hopkins <thematthopkins@gmail.com>
2021-08-23 15:47:39 -0400

    wip

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+class OrderHandler
+  attr_accessor :events
+
+  def initialize
+    @events = []
+  end
+
+  def call(event)
+    puts "called with event #{event}"
+    events.append(event)
+  end
+end
+
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'calls subscribed notifications on event publication' do
+    event_store = Rails.configuration.event_store
+    orderHandler = OrderHandler.new
+    event_store.subscribe(orderHandler, to: [CreateCart])
+    event = CreateCart.new(
+      data: {
+        id: 'my-cart-id'
+      }
+    )
+    event_store.publish(event, stream_name: 'my-stream', expected_version: 10)
+    expect(orderHandler.events)
+      .to eq([event])
+  end
 end
