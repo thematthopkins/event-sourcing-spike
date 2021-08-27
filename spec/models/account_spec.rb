875153de02564fdb9b5e3fbf19974d9cc964f1fc
Matthew Hopkins <thematthopkins@gmail.com>
2021-08-27 16:20:55 -0400

    optmistic locking test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,10 +1,19 @@
 # typed: strict
 require 'rails_helper'
+require 'benchmark'
+
 
 class AccountCreated < RailsEventStore::Event
+  sig {params(msg: {data:{name: String, id: String}}).returns(AccountCreated)}
+  def initialize(msg)
+    base(msg)
+  end
 end
   
 class AccountNameChanged < RailsEventStore::Event
+end
+
+class AccountNameChangeCompleted < RailsEventStore::Event
 end
 
 class AccountAggregate
@@ -17,7 +26,11 @@ class AccountAggregate
   end
 
   def changeName(new_name)
-    apply AccountNameChanged.new(data: {name: new_name, prev_name: @name})
+    apply(AccountNameChanged.new(data: {name: new_name, prev_name: @name}))
+  end
+
+  def changeName2(new_name)
+    apply(AccountNameChanged.new(data: {name: new_name, prev_name: @name}), AccountNameChangeCompleted.new())
   end
 
   on AccountNameChanged do |event|
@@ -29,6 +42,9 @@ class AccountAggregate
     @name = event.data[:name]
     @id = event.data[:id]
   end
+
+  on AccountNameChangeCompleted do |event|
+  end
 end
 
 
@@ -37,8 +53,33 @@ RSpec.describe Account, type: :model do
 
   let (:repository) { AggregateRoot::Repository.new }
 
+  it 'optimistic locking works with different event counts' do
+    expect {
+      account_id = '1'
+      t = Thread.new do
+        repository.with_aggregate(AccountAggregate.new, account_id) do |account|
+          puts 'changine name b'
+          sleep 0.1
+          account.changeName('b')
+          puts 'changed name b'
+        end
+      end
+
+
+      repository.with_aggregate(AccountAggregate.new, account_id) do |account|
+        puts 'changine name a'
+        sleep 1
+        account.changeName2('a')
+        puts 'changed name a'
+      end
+
+      puts 'waiting'
+      t.join
+    }.to raise_exception(RubyEventStore::WrongExpectedEventVersion)
+  end
+
   it 'can process bulk name change events' do
-    num_accounts = 10
+    num_accounts = 1
 
     account_ids = (0...num_accounts).map{ |i| SecureRandom.uuid }
     account_ids.each{ |account_id|
@@ -49,7 +90,7 @@ RSpec.describe Account, type: :model do
         }
       ), stream_name: account_id)
     }
-    num_name_changes = 10
+    num_name_changes = 20
     name_changes = (0...num_name_changes).map{ |i| SecureRandom.uuid }
 
     account_ids.each{|account_id|
@@ -60,16 +101,20 @@ RSpec.describe Account, type: :model do
       }
     }
 
-    end_states =
-      account_ids.map{|account_id|
-        repository.load(AccountAggregate.new, account_id)
-      }
+    time = Benchmark.measure {
 
-    expect(end_states.length) .to eq(num_accounts)
-    end_states.each_with_index {|account, i| 
-      expect(account.id) .to eq(account_ids[i])
-      expect(account.name) .to eq(name_changes[-1])
-      expect(account.prev_name) .to eq(name_changes[-2])
+      end_states =
+        account_ids.map{|account_id|
+          repository.load(AccountAggregate.new, account_id)
+        }
+
+      expect(end_states.length) .to eq(num_accounts)
+      end_states.each_with_index {|account, i| 
+        expect(account.id) .to eq(account_ids[i])
+        expect(account.name) .to eq(name_changes[-1])
+        expect(account.prev_name) .to eq(name_changes[-2])
+      }
     }
+    puts "projection time: #{time.real}"
   end
 end
